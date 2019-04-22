ebc6db1ba2b88206f6e1c4265a4c5207dd479ce7
Süleyman Fakir <suleyman.fakir@htmodeme.com>
2019-04-22 16:41:38 +0300

    replace with list on received page

--- src/pages/Wallets/BitcoinWalletReceive.js
+++ src/pages/Wallets/BitcoinWalletReceive.js
@@ -1,43 +1,54 @@
 import React from 'react';
-import { Table, Button } from 'antd';
+import _ from 'lodash';
+import { List, Row, Col, Icon, Button, Input, Divider, Badge } from 'antd';
 
 const dataSource = [
   {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
+    name: 'From Web Sales',
+    uuid: '5b47dc48-6501-11e9-a923-1681be663d3e',
   },
   {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    name: 'Label this address',
+    uuid: '72a6d01a-6501-11e9-a923-1681be663d3e',
   },
 ];
 
 const BitcoinWalletReceive = () => {
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} showHeader={false} pagination={false} />
+      <List
+        dataSource={dataSource}
+        renderItem={(item, index) => {
+          return (
+            <List.Item>
+              <Row type="flex" justify="space-around" style={{ width: '100%' }}>
+                <Col span={10} style={{ display: 'flex', alignItems: 'center' }}>
+                  {_.size(dataSource) !== 1 && (
+                    <Badge
+                      count={index + 1}
+                      style={{
+                        marginRight: 10,
+                        backgroundColor: '#fff',
+                        color: '#999',
+                        boxShadow: '0 0 0 1px #d9d9d9 inset',
+                      }}
+                    />
+                  )}
+                  {item.name}
+                </Col>
+                <Col span={10}>
+                  <Input addonAfter={<Icon type="setting" />} defaultValue={item.uuid} />
+                </Col>
+                <Col span={4} style={{ display: 'flex', alignItems: 'center' }}>
+                  <Icon type="edit" theme="filled" style={{ paddingLeft: 10, paddingRight: 10 }} />
+                  <Divider type="vertical" />
+                  <Icon type="setting" theme="filled" style={{ paddingRight: 10 }} />
+                </Col>
+              </Row>
+            </List.Item>
+          );
+        }}
+      />
       <div style={{ paddingTop: 10, textAlign: 'center' }}>
         <Button>Create new address for your Bitcoin Wallet</Button>
       </div>
