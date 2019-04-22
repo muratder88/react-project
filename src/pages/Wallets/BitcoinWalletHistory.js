e7c35130584bcb96eff8f33837c21560f5b46ad1
Süleyman Fakir <suleyman.fakir@htmodeme.com>
2019-04-22 16:13:08 +0300

    use list insted of table on history

--- src/pages/Wallets/BitcoinWalletHistory.js
+++ src/pages/Wallets/BitcoinWalletHistory.js
@@ -1,41 +1,50 @@
 import React from 'react';
-import { Table } from 'antd';
+import { List, Row, Col, Icon } from 'antd';
 
 const dataSource = [
   {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
+    code: 'APR09',
+    name: 'Sold BTC for Etherreum',
+    type: 'Sold',
+    price: [{ key: '1', value: '-0.000012 BTC' }, { key: '2', value: '300' }],
   },
   {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
+    code: 'APR02',
+    name: 'Bought BTC with Bardays VISA',
+    type: 'Sold',
+    price: [{ key: '1', value: '0.2 BTC' }, { key: '2', value: '1,715.20' }],
   },
 ];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
+const getIconType = type => {
+  switch (type) {
+    case 'Sold':
+      return 'edit';
+    default:
+      return 'form';
+  }
+};
 
 const BitcoinWalletHistory = () => (
-  <Table dataSource={dataSource} columns={columns} showHeader={false} pagination={false} />
+  <List
+    dataSource={dataSource}
+    renderItem={item => {
+      return (
+        <List.Item>
+          <Row type="flex" justify="space-around" style={{ width: '100%' }}>
+            <Col span={4}>{item.code}</Col>
+            <Col span={16}>
+              <Icon type={getIconType(item.type)} theme="filled" style={{ paddingRight: 10 }} />
+              {item.name}
+            </Col>
+            <Col span={4} style={{ textAlign: 'right' }}>
+              <div>{item.price[0].value}</div>
+              <div>{item.price[1].value}</div>
+            </Col>
+          </Row>
+        </List.Item>
+      );
+    }}
+  />
 );
 
 export default BitcoinWalletHistory;
