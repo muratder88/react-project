6878c706cfc062fb65f08e9d062745562ff28e67
Süleyman Fakir <suleyman.fakir@htmodeme.com>
2019-04-23 21:00:14 +0300

    change firstModal design

--- src/pages/Wallets/FirstModal.js
+++ src/pages/Wallets/FirstModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Modal } from 'antd';
+import { Modal, Row, Col, Divider, Icon, Button } from 'antd';
+
+import styles from './FirstModal.less';
 
 const FirstModal = ({ isModalVisible, dispatch }) => {
   console.log('First Modal is rendering ');
@@ -14,14 +16,53 @@ const FirstModal = ({ isModalVisible, dispatch }) => {
 
   return (
     <Modal
-      title="Basic Modal"
       visible={isModalVisible}
       onOk={handleModalOk}
       onCancel={handleModalOk}
+      footer={null}
+      closable={false}
+      centered
+      width={689}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <div>
+        <div style={{marginRight: 50}}>
+          <h2>You have <span style={{fontWeight: 'bold'}}>sent 0.23397 BTC</span> ≈£937.46</h2>
+          <Row>
+            <Col span={12}>Date</Col>
+            <Col span={12} style={{textAlign: "right"}}>14.04.2019 20:33</Col>
+          </Row>
+          <Divider className={styles.divider} />
+          <Row>
+            <Col span={12}>Note</Col>
+            <Col span={12} style={{textAlign: "right"}}>Thanks for great product</Col>
+          </Row>
+          <Divider className={styles.divider} />
+          <Row>
+            <Col span={12}>Receiver</Col>
+            <Col span={12} style={{textAlign: "right"}}>779c3c53-57be-4b7f-bee6-0a127cc8fe2d</Col>
+          </Row>
+          <Divider className={styles.divider} />
+          <Row>
+            <Col span={12}>Miner Fee</Col>
+            <Col span={12} style={{textAlign: "right"}}>0.00014105 BTC</Col>
+          </Row>
+          <Divider className={styles.divider} />
+          <Row>
+            <Col span={12}>Mintus Fee</Col>
+            <Col span={12} style={{textAlign: "right"}}>-</Col>
+          </Row>
+          <Divider className={styles.divider} />
+          <Row>
+            <Col span={12}>Total</Col>
+            <Col span={12} style={{textAlign: "right"}}>0.23461139 BTC</Col>
+          </Row>
+        </div>
+        <div style={{position: 'absolute', right: '0', top: 0, width: 50}}>
+          <a onClick={handleModalOk}>
+            <Icon type="close" style={{color: '#000000', marginTop: 20, fontSize: 36}} />
+          </a>
+        </div>
+      </div>
     </Modal>
   );
 };
